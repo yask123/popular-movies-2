450253cc52ad6fc3f8213ff3b191f1b64f7feb19
Yask Srivastava <yask123@gmail.com>
2016-06-14 19:43:02 +0530

    Most things work now

--- app/src/main/java/com/example/yask/newmoviesexplorer/IMDB_Client.java
+++ app/src/main/java/com/example/yask/newmoviesexplorer/IMDB_Client.java
@@ -1,7 +1,5 @@
 package com.example.yask.newmoviesexplorer;
 
-import android.util.Log;
-
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -13,7 +11,7 @@ import java.util.HashMap;
  */
 
 public class IMDB_Client {
-    private  final String KEY = "05df3644ee3e75b407700ca976b07874";
+    private final String KEY = "";
     private final String BASE_URL = "http://api.themoviedb.org/3/discover/movie";
     private AsyncHttpClient client;
 
@@ -28,18 +26,16 @@ public class IMDB_Client {
     public void getMovies(String selected_option, JsonHttpResponseHandler handler) {
         String url = getURL("/");
         HashMap<String, String> paramMap = new HashMap<String, String>();
-        paramMap.put("sort_by", selected_option);
         paramMap.put("api_key", KEY);
         RequestParams params = new RequestParams(paramMap);
 
-        Log.e("test, url", "http://api.themoviedb.org/3/discover/movie/?api_key=" + KEY + "&sort_by=" + selected_option + ".desc");
         client.get("http://api.themoviedb.org/3/movie/" + selected_option + "?api_key=" + KEY, handler);
     }
 
 
     public void getTrailer(String ID, JsonHttpResponseHandler handler){
         RequestParams params = new RequestParams("api_key",KEY);
-        Log.e("URL","http://api.themoviedb.org/3/movie/"+ID+"/videos");
+
         client.get("http://api.themoviedb.org/3/movie/"+ID+"/videos",params,handler);
 
     }
